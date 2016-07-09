e3d94899185eec3d7547c959a02281599d96e0fd
TimMagoun <timmagoun@gmail.com>
2016-07-08 22:50:54 -0400

    Added problem 1 to the set

---
+++ src/Modulus.java
@@ -0,0 +1,22 @@
+public class Modulus {
+
+	public static void main(String[] args) {
+		int dividend = 52;
+		int divisor = 10;
+		int remainder = 0;
+		
+		//Write code here...
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//This should print out "2"
+		System.out.println(remainder);
+	}
+
+}
