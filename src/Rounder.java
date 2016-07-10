af3cb7dfe5c8be70ff626e401e79be158098b393
Aravind Koneru <aravind.b.koneru@gmail.com>
2016-07-10 14:15:33 -0400

    added another problem to problem set 1

---
+++ src/Rounder.java
@@ -0,0 +1,20 @@
+public class Rounder{
+
+	public static void main(String[] args){
+
+		double value_to_round = 15.2;
+		int rounded_value;
+
+		//Insert Code Here....
+
+
+
+
+
+		//Should print out 15
+		System.out.println(rounded_value);
+
+
+	}
+
+}
