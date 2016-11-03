907c1dc55c77c794d59dd484d07b0ea7652dbcc8
Tim Magoun <timmagoun@gmail.com>
2016-11-03 08:28:40 -0400

    Added superclass that was previously deleted

---
+++ src/Counter.java
@@ -0,0 +1,37 @@
+
+public class Counter{
+	
+	protected int value;
+	protected int maxValue;
+	protected int minValue;
+
+	public Counter(){
+
+		value = 10;
+		maxValue = 100;
+		minValue = 0;
+
+	}
+
+	public Counter(int maxValue, int minValue, int value){
+
+		this.value = value;
+		this.maxValue = maxValue;
+		this.minValue = minValue;
+
+	}
+
+	public boolean countUp(){
+		value++;
+		return checkBounds();
+	}
+
+	public boolean countDown(){
+		value--;
+		return checkBounds();
+	}
+
+	public boolean checkBounds(){
+		return (value>=minValue||value<=maxValue);
+	}
+}
